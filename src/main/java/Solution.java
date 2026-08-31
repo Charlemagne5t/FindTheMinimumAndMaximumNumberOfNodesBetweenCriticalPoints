1e3bf562531b93b9c7cfb9294dd02add067b54d2
alek <wait82985@gmail.com>
2026-08-31 13:46:39 +0500

    problem solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,29 +1,42 @@
-public class Solution {
+class Solution {
     public int[] nodesBetweenCriticalPoints(ListNode head) {
-        int[] res = {-1, -1};
-
-        ListNode temp = head;
+        int l = -1;
+        int r = -1;
+        int c = 1;
+        int[] res = new int[2];
+        res[0] = Integer.MAX_VALUE;
+        res[1] = -1;
         int prev = head.val;
-        int first = -1;
-        int last = -1;
         head = head.next;
-        int count = 1;
-        while(head.next != null) {
-            if((head.val > prev && head.val > head.next.val) || (head.val < prev && head.val < head.next.val)) {
-
-                if(first == -1) {
-                    first = count;
-                }else {
-                    res[0] = res[0] == -1 ? count - first : Math.min(res[0], count - last);
-                    res[1] = count - first;
-                }
-                last = count;
-            }
-            prev = head.val;
-            count++;
-            head = head.next;
+        if (head == null) {
+            return new int[]{-1, -1};
         }
+        int prevR = -1;
+        while (head.next != null) {
+            if (head.val > prev && head.val > head.next.val ||
+                    head.val < prev && head.val < head.next.val) {
+                if (l == -1) {
+                    l = c;
+                    prevR = c;
+                } else {
+                    prevR = r == -1 ? prevR : r;
+                    r = c;
+                }
+                if (l != -1 && r != -1) {
+                    if (prevR != -1) {
+                        res[0] = Math.min(res[0], r - prevR);
+                    }
 
-        return res;
+                    res[1] = Math.max(res[1], r - l);
+                }
+            }
+            c++;
+            prev = head.val;
+            head = head.next;
+
+        }
+        if (res[1] != -1) {
+            return res;
+        } else return new int[]{-1, -1};
     }
 }
